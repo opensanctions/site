b0c169de1aa41570fe64c9b478cf6cf0246dcb9e
Friedrich Lindenberg <friedrich@pudo.org>
2022-01-31 08:09:18 +0100

    update measurement ID?

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,18 +1,18 @@
 // @ts-nocheck
-export const GA_TRACKING_ID = 'G-H9SZPZKV6Y'
+export const GA_TRACKING_ID = 'UA-34039573-17'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-    window.gtag('config', GA_TRACKING_ID, {
-        page_path: url,
-    })
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }) => {
-    window.gtag('event', action, {
-        event_category: category,
-        event_label: label,
-        value: value,
-    })
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
 }
