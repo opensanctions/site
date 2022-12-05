2f0aa4ed1043a6c8e2da0ac65a87c839bf25faed
Friedrich Lindenberg <friedrich@pudo.org>
2022-12-05 15:02:49 +0100

    try a different way to force this to be dynamic

--- app/recent/[name]/page.tsx
+++ app/recent/[name]/page.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation";
+import { getDatasets } from "../../../lib/data";
 
 type RecentPageProps = {
   params: { name: string }
@@ -7,4 +8,12 @@ type RecentPageProps = {
 export default function Page({ params }: RecentPageProps) {
   redirect(`/datasets/${params['name']}/#recents`)
   return null;
+}
+
+export async function generateStaticParams() {
+  const datasets = await getDatasets();
+
+  return datasets.map((ds) => ({
+    name: ds.name,
+  }));
 }
