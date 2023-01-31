5587bdde9cad53d4a7ee661e65f1b65076a52a20
etlel <plmoell@hotmail.com>
2023-01-30 23:11:07 -0500

    Update Layout.tsx
    opensearch plugin

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -33,6 +33,7 @@ function LayoutBase({ title, description, imageUrl, structured, activeSection, c
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="search" type="application/opensearchdescription+xml" title="OpenSanctions" href="/opensearch.xml">
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@open_sanctions" />
         <meta name="twitter:creator" content="@pudo" />
