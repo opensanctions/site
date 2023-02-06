298a739c9f4fd4decb4d78e207c2e263c06d0416
Friedrich Lindenberg <friedrich@pudo.org>
2023-02-06 18:45:42 +0100

    clean up preview header

--- app/entities/preview/[id]/head.tsx
+++ app/entities/preview/[id]/head.tsx
@@ -1,21 +1,14 @@
 import PageHead from "../../../../components/layout/PageHead";
-import { getEntity, isBlocked, isIndexRelevant } from '../../../../lib/data';
+import { getEntity, isBlocked } from '../../../../lib/data';
 import { EntityPageProps } from '../../[id]/common';
 
 
 export default async function Head({ params }: EntityPageProps) {
   const entity = await getEntity(params.id);
-  if (entity == null) {
+  if (entity === null) {
     return <PageHead title="Loading..." />;
   }
-  const noIndex = !isIndexRelevant(entity);
   const title = isBlocked(entity) ? 'Blocked entity' : entity.caption;
-  const entityUrl = `/entities/${entity.id}/`;
-  return (
-    <>
-      <PageHead title={title} noIndex={noIndex} />
-      <link rel="canonical" href={entityUrl} />
-    </>
-  );
+  return <PageHead title={title} noIndex={true} />;
 }
 
