6f83b124ed87c12ddd2d93f0cd950bfbaaa8f9bf
Friedrich Lindenberg <friedrich@pudo.org>
2023-02-09 15:54:03 +0100

    change headline

--- app/kyb/common.tsx
+++ app/kyb/common.tsx
@@ -1,2 +1,2 @@
 export const TITLE = "Know-your-business datasets";
-export const SUMMARY = "OpenSanctions KYB is a group of datasets related to corporate ownership and control. They are published in the FollowTheMoney format used by OpenSanctions and can be used to build complex knowledge graphs.";
+export const SUMMARY = "OpenSanctions KYB is a group of datasets related to corporate ownership and control. They are published in the FollowTheMoney data format and can be used to build complex knowledge graphs.";
