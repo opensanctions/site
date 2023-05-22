a48b42e23786344759a5c49882e6e567244e1b11
Éléonore Mayola <eleonore.mayola@gmail.com>
2023-05-22 21:56:39 +0200

    Trying to fix oauth error

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,6 +8,8 @@ export const authOptions: NextAuthOptions = {
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+      id: process.env.AUTH0_ID,
     }),
   ],
   callbacks: {
