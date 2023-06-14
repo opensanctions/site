e02a3240e1ce06dc056bca91c36dc5951aa216fc
JD Bothma <jbothma@gmail.com>
2023-06-14 22:15:05 +0200

    Fix up valid element nesting but...
    it's looking like forking the component might make more sense

--- components/DatasetCountryListing.tsx
+++ components/DatasetCountryListing.tsx
@@ -14,43 +14,50 @@ type DatasetCountryListingProps = {
   defaultLimit?: number | null
 }
 
-export default function DatasetCountryListing({ datasetName, countries, defaultExpanded=false, defaultLimit=null }: DatasetCountryListingProps) {
+export default function DatasetCountryListing({ datasetName, countries, defaultExpanded = false, defaultLimit = null }: DatasetCountryListingProps) {
   const [coverageExpanded, setCoverageExpanded] = useState(defaultExpanded);
   const [limit, setLimit] = useState(defaultLimit);
-  const visibleCountries = (typeof(limit) == "number") ? countries.slice(0, limit-1) : countries;
+  const visibleCountries = (typeof (limit) == "number") ? countries.slice(0, limit - 1) : countries;
 
   return (
-    <Table size="sm" className="inner-table">
-      <thead>
-        <tr>
-          <td colSpan={2}>
-            <Plural value={countries.length} one="country" many="countries" />
-            <HelpLink href={`/reference/#type.country`} />
-            <Spacer />
-            {coverageExpanded && !defaultExpanded && (
-              <a onClick={(e) => { e.preventDefault(); setCoverageExpanded(false) }} href='#'>Hide overview...</a>
-            )}
-            {!coverageExpanded && (
-              <a onClick={(e) => { e.preventDefault(); setCoverageExpanded(true) }} href='#'>Show overview...</a>
-            )}
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-        {coverageExpanded && visibleCountries.map(c =>
-          <tr key={c.code}>
-            <td>
-              <a href={`/search/?scope=${datasetName}&countries=${c.code}`}>
-                {c.label}
-              </a>
+    <>
+      <Table size="sm" className="inner-table">
+        <thead>
+          <tr>
+            <td colSpan={2}>
+              <Plural value={countries.length} one="country" many="countries" />
+              <HelpLink href={`/reference/#type.country`} />
+              {coverageExpanded && !defaultExpanded && (
+                <>
+                  <Spacer />
+                  <a onClick={(e) => { e.preventDefault(); setCoverageExpanded(false) }} href='#'>Hide overview...</a>
+                </>
+              )}
+              {!coverageExpanded && (
+                <>
+                  <Spacer />
+                  <a onClick={(e) => { e.preventDefault(); setCoverageExpanded(true) }} href='#'>Show overview...</a>
+                </>
+              )}
             </td>
-            <td className="numeric"><Numeric value={c.count} /></td>
           </tr>
-        )}
-        {coverageExpanded && limit !== null  && (
-              <a onClick={(e) => { e.preventDefault(); setLimit(null) }} href='#'>Show all...</a>
-            )}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {coverageExpanded && visibleCountries.map(c =>
+            <tr key={c.code}>
+              <td>
+                <a href={`/search/?scope=${datasetName}&countries=${c.code}`}>
+                  {c.label}
+                </a>
+              </td>
+              <td className="numeric"><Numeric value={c.count} /></td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      {coverageExpanded && limit !== null && (
+        <a onClick={(e) => { e.preventDefault(); setLimit(null) }} href='#'>Show all...</a>
+      )}
+    </>
   )
 }
