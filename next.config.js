e9ba0d152b2ff4304f929dcad441417681e36fd9
Friedrich Lindenberg <friedrich@pudo.org>
2023-06-26 13:18:42 +0200

    cleanup

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    // appDir: true,
     serverComponentsExternalPackages: ['react-bootstrap']
   },
   staticPageGenerationTimeout: 360,
