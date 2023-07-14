f3f670dff693f9c5c561b90622cedfdf861d275c
JD Bothma <jbothma@gmail.com>
2023-07-14 17:05:03 +0200

    Start mocking out country pages with PEP position mapping

---
+++ app/countries/[slug]/page.tsx
@@ -0,0 +1,124 @@
+import { Col, Container, Row } from "../../../components/wrapped";
+import LayoutFrame from "../../../components/layout/LayoutFrame";
+import { getGenerateMetadata } from "../../../lib/meta";
+import { REVALIDATE_BASE } from "../../../lib/constants";
+import { Plus } from 'react-bootstrap-icons';
+import { Spacer } from "../../../components/util";
+
+export const revalidate = REVALIDATE_BASE;
+
+export async function generateMetadata() {
+  return getGenerateMetadata({
+    title: `Data available for Russia`
+  })
+}
+
+export default async function Page() {
+  return (
+    <LayoutFrame>
+      <Container>
+        <h1>
+          Russia
+        </h1>
+        <Row>
+          <Col md={3}>
+            <p>
+              <strong>Sanctions</strong> data for 12,345 entities connected with <strong>Russia</strong> are availble from 123 sources.
+            </p>
+          </Col>
+          <Col md={9}>
+            something something truncated list of datasets?
+          </Col>
+        </Row>
+        <hr className="d-print-none" />
+        <Row>
+          <Col md={3}>
+            <p>
+              <strong>Politically-exposed persons (PEP)</strong> data is available for 12,345 persons connected with <strong>Russia</strong> from 123 sources.
+            </p>
+          </Col>
+          <Col md={9}>
+            <table className="table table-responsive">
+              <thead>
+                <tr>
+                  <th/>
+                  <th/>
+                  <th colSpan={3}>
+                    Number of known occupants
+                  </th>
+                </tr>
+                <tr>
+                  <th/>
+                  <th>Category</th>
+                  <th>Current</th>
+                  <th>Ended</th>
+                  <th>Unsure</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="clickable" data-toggle="collapse" data-target="#group-of-rows-1">
+                  <td><Plus/></td>
+                  <td>Office of the head of national government</td>
+                  <td>4</td>
+                  <td>13</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+              <tbody id="group-of-rows-1" className="">
+                <tr>
+                  <tr/>
+                  <td>President of Russia</td>
+                  <td>1</td>
+                  <td>2</td>
+                  <td>0</td>
+                </tr>
+                <tr>
+                  <tr/>
+                  <td>Vice President of Russia</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                </tr>
+                <tr>
+                  <tr/>
+                  <td>Prime Minister of Russia</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr className="clickable" data-toggle="collapse" data-target="#group-of-rows-2">
+                  <td><Plus/></td>
+                  <td>Executive branch of the national government</td>
+                  <td>4</td>
+                  <td>13</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr className="clickable" data-toggle="collapse" data-target="#group-of-rows-3">
+                  <td><Plus/></td>
+                  <td>Legislative branch of the national government</td>
+                  <td>4</td>
+                  <td>13</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+              <tbody>
+                <tr className="clickable" data-toggle="collapse" data-target="#group-of-rows-3">
+                  <td><Plus/></td>
+                  <td>Office of the head of regional government</td>
+                  <td>4</td>
+                  <td>13</td>
+                  <td>3</td>
+                </tr>
+              </tbody>
+            </table>
+          </Col>
+
+        </Row>
+      </Container>
+    </LayoutFrame>
+  )
+}
